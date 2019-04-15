1966bd0c1dc873d190c90e3c68c65b5ac5f98d62
wangzhichao <marsak@live.cn>
2019-04-15 17:33:48 +0800

    v0.6.8

--- components/comment.vue
+++ components/comment.vue
@@ -1,87 +1,163 @@
 <template>
-    <div>
-    <a-list
-      v-if="comments.length"
-      :dataSource="comments"
-      :header="`共 ${comments.length} ${comments.length > 1 ? '条评论' : '条评论'}`"
-      itemLayout="horizontal"
-    >
-      <a-list-item slot="renderItem" slot-scope="item, index">
-        <a-comment
-          :author="item.author"
-          :avatar="item.avatar"
-          :content="item.content"
-          :datetime="item.datetime"
-        >
-        </a-comment>
-      </a-list-item>
-    </a-list>
-    <a-comment>
-      <a-avatar
-        slot="avatar"
-        :src="$withBase('/images/cgqz.jpg')"
-        alt="吃瓜群众"
-      ></a-avatar>
-      <div slot="content">
-        <a-form-item>
-          <a-textarea 
-          :rows="4" 
-          @change="handleChange" 
-          :value="value"
-          placeholder="暂无服务器存储数据，所以目前可以随便玩" ></a-textarea>
-        </a-form-item>
-        <a-form-item>
-          <a-button
-            htmlType="submit"
-            :loading="submitting"
-            @click="handleSubmit"
-            type="primary"
-          >
-            添加评论
-          </a-button>
-        </a-form-item>
-      </div>
-    </a-comment>
+<div class="comment">
+  <div class="post-bottom">
+    <div class="col">
+      <router-link 
+      to="#"
+      class="like">
+        <a-icon type="heart" />
+        <span>0 赞</span>
+      </router-link>
+    </div>
+    <div class="col">
+      <a-icon type="eye" />
+      <span 
+      class="leancloud-visitors" 
+      :id="'/'+this.$page.frontmatter.title+'.html'"
+      data-flag-title="文章阅读量统计">
+        <i class="leancloud-visitors-count">121</i> 
+        <em class="post-meta-item-text">查看</em>
+      </span>
+    </div>
+    <!-- <div class="col">
+      <a-icon type="message" />
+      <span>没有评论</span>
+    </div> -->
+    <div class="post-nav">
+      <router-link 
+      :to="this.$page.frontmatter.prev"
+      class="prev"
+      v-if="this.$page.frontmatter.prev">
+        <a-icon type="left" />
+        <span>上一篇</span>
+      </router-link>
+      <router-link 
+      :to="this.$page.frontmatter.next"
+      class="next"
+      v-if="this.$page.frontmatter.next">
+        <a-icon type="right" />
+        <span>下一篇</span>
+      </router-link>
+    </div>
   </div>
+  <div class="vcomment">
+    <div id="vcomments"></div>
+  </div>
+</div>
 </template>
 <script>
-import moment from 'moment'
-import 'moment/locale/zh-cn'
+
 export default {
+  computed: {
     data () {
-    return {
-      comments: [],
-      submitting: false,
-      value: '',
-      moment,
-    }
-  },
-  methods: {
-    handleSubmit() {
-      if (!this.value) {
-        return;
-      }
-
-      this.submitting = true
-
-      setTimeout(() => {
-        this.submitting = false
-        this.comments = [
-          {
-            author: '吃瓜群众',
-            avatar: `../images/cgqz.jpg`,
-            content: this.value,
-            datetime: moment().fromNow(),
-          },
-          ...this.comments,
-        ]
-        this.value = ''
-      }, 1000)
+      return this.$site.themeConfig
     },
-    handleChange(e) {
-      this.value = e.target.value
+  },
+  mounted: function(){
+    this.createValine()
+  },
+  
+  methods: {
+    createValine() {
+      const Valine = require('valine');
+      window.AV = require('leancloud-storage')
+      const valine =  new Valine({
+        el: '#vcomments',
+        appId: this.data.id,
+        appKey: this.data.key,
+        notify: true,
+        verify: false,
+        avatar: 'wavatar',
+        visitor:true,
+        path: window.location.pathname,
+        placeholder: '欢迎留言与我分享您的想法...',
+      });
+      this.valineRefresh = false
     }
   },
+  watch: {
+    '$route' (to, from) {
+      if(to.path !==  from.path){
+        setTimeout(() => {
+          //重新刷新valine
+          this.createValine()
+        }, 180)
+      }
+    }
+  }
 }
 </script>
+<style lang="less" scoped>
+.comment{
+  .post-bottom{
+    overflow: hidden;
+    border-top: 1px solid #323232;
+    padding: 20px 0;
+    font-size: 12px;
+    text-transform: uppercase;
+    .col{
+      display: inline-block;
+      margin-right: 20px;
+      vertical-align: middle;
+      margin: 4px 15px 4px 0;
+      *{
+        display: inline-block;
+        vertical-align: middle;
+      }
+      .like{
+        float: left;
+        text-decoration: none;
+        background: #FF715B;
+        font-size: 12px;
+        padding: 5px 10px;
+        color: #fff;
+        i{
+          font-size: 16px;
+          margin-right: 5px;
+        }
+      }
+      span{
+        font-weight: lighter;
+        i,em{
+          float:left;
+          font-style: normal;
+        }
+      }
+    }
+    .post-nav{
+      float: right;
+      margin: 3px 0;
+      a{
+        background: #282828;
+        color: #fff;
+        text-decoration: none;
+        padding: 2px 10px;
+        display: inline-block;
+        vertical-align: middle;
+        *{
+          display: inline-block;
+          vertical-align: middle;
+        }
+        i{
+          position: relative;
+          z-index: 1;
+          font-size: 9px;
+        }
+        span{
+          font-size: 12px;
+          font-weight: bold;
+          text-transform: uppercase;
+          margin-left: 3px;
+          font-weight: lighter;
+        }
+      }
+    }
+  }
+  #vcomments {
+    display:block;
+    margin-left:auto;
+    margin-right:auto;
+  }
+}
+</style>
 
--- layouts/post.vue
+++ layouts/post.vue
@@ -13,40 +13,6 @@
             </div>
             <div class="container">   
                 <Content class="content"/>
-                <div class="post-bottom">
-                    <div class="col">
-                        <router-link 
-                        to="#"
-                        class="like">
-                            <a-icon type="heart" />
-                            <span>0 赞</span>
-                        </router-link>
-                    </div>
-                    <div class="col">
-                        <a-icon type="eye" />
-                        <span>121 查看</span>
-                    </div>
-                    <div class="col">
-                        <a-icon type="message" />
-                        <span>没有评论</span>
-                    </div>
-                    <div class="post-nav">
-                        <router-link 
-                        :to="this.$page.frontmatter.prev"
-                        class="prev"
-                        v-if="this.$page.frontmatter.prev">
-                            <a-icon type="left" />
-                            <span>上一篇</span>
-                        </router-link>
-                        <router-link 
-                        :to="this.$page.frontmatter.next"
-                        class="next"
-                        v-if="this.$page.frontmatter.next">
-                            <a-icon type="right" />
-                            <span>下一篇</span>
-                        </router-link>
-                    </div>
-                </div>
                 <Comment></Comment>
             </div>
             <Catalo></Catalo>
@@ -122,66 +88,6 @@ export default {
             text-align: justify;
             .content{
                 margin-bottom: 35px;
-            }
-            .post-bottom{
-                overflow: hidden;
-                border-top: 1px solid #323232;
-                padding: 20px 0;
-                font-size: 12px;
-                text-transform: uppercase;
-                .col{
-                    display: inline-block;
-                    margin-right: 20px;
-                    vertical-align: middle;
-                    margin: 4px 15px 4px 0;
-                    *{
-                        display: inline-block;
-                        vertical-align: middle;
-                    }
-                    .like{
-                        float: left;
-                        text-decoration: none;
-                        background: #FF715B;
-                        font-size: 12px;
-                        padding: 5px 10px;
-                        color: #fff;
-                        i{
-                            font-size: 16px;
-                            margin-right: 5px;
-                        }
-                    }
-                    span{
-                        font-weight: lighter;
-                    }
-                }
-                .post-nav{
-                    float: right;
-                    margin: 3px 0;
-                    a{
-                        background: #282828;
-                        color: #fff;
-                        text-decoration: none;
-                        padding: 2px 10px;
-                        display: inline-block;
-                        vertical-align: middle;
-                        *{
-                            display: inline-block;
-                            vertical-align: middle;
-                        }
-                        i{
-                            position: relative;
-                            z-index: 1;
-                            font-size: 9px;
-                        }
-                        span{
-                            font-size: 12px;
-                            font-weight: bold;
-                            text-transform: uppercase;
-                            margin-left: 3px;
-                            font-weight: lighter;
-                        }
-                    }
-                }
             }
         }
         .container:before,.container:after{
